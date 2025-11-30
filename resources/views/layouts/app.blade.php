780668d265369ca1d2ff66aa8d3432096f91b34f
ramziddin <mac@MacBook-Air-Mac.local>
2025-11-30 21:23:15 +0100

    added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,9 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <!-- Scripts -->
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
         @stack('styles')
 </head>
 <body style="background-color: rgb(220, 213, 213)">
@@ -159,11 +161,7 @@
         </main>
     </div>
     
-    
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     @yield('scripts')
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
