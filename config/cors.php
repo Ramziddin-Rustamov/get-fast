65c54189819b62fd9a8cc80b396faa320ba87d6f
Ramziddin Rustamov <rustamovramziddin7@gmail.com>
2026-04-29 11:55:15 +0200

    adeeeed

--- config/cors.php
+++ config/cors.php
@@ -10,6 +10,7 @@ return [
     'allowed_origins' => [
         'https://yashil-yol.vercel.app',
         'https://ketamiz.com',
+        'https://0d01-91-196-77-111.ngrok-free.app',
     ],
 
     'allowed_origins_patterns' => [],
@@ -23,8 +24,3 @@ return [
     'supports_credentials' => true,
 
 ];
-
-
-
-
-?>
